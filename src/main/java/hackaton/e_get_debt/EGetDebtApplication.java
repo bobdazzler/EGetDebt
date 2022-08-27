9d589b80540f1c3579741eb605f906f8e4b1f2f6
bobdazzler <Oghorobob93@gmail.com>
2022-08-27 13:01:37 +0100

    naming convention followed

---
+++ src/main/java/hackaton/e_get_debt/EGetDebtApplication.java
@@ -0,0 +1,13 @@
+package Hackaton.E_Get_Debt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGetDebtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGetDebtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hackaton/E_Get_Debt/EGetDebtApplicationTests.java
@@ -0,0 +1,13 @@
+package Hackaton.E_Get_Debt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGetDebtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
